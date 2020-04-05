43bdca108c1b0eca77ddf2ac860dd3183a2d0782
Weronika Miller <weronika.altowka@gmail.com>
2020-04-05 11:32:08 -0700

    web server

---
+++ bin/main.dart
@@ -0,0 +1,19 @@
+import 'dart:io' show Platform;
+import 'dart:async' show runZoned;
+import 'package:path/path.dart' show join, dirname;
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
+
+void main() {
+  var pathToBuild = join(dirname(Platform.script.toFilePath()), '..', 'build');
+
+  var handler = createStaticHandler(pathToBuild, defaultDocument: 'index.html');
+
+  var portEnv = Platform.environment['PORT'];
+  var port = portEnv == null ? 9999 : int.parse(portEnv);
+
+  runZoned(() {
+    io.serve(handler, '0.0.0.0', port);
+    print('Serving $pathToBuild on port $port');
+  }, onError: (e, stackTrace) => print('Oh noes! $e $stackTrace'));
+}
